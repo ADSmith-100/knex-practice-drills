f1e8d6f19d5b180a5999081013ae18cf249fd6b0
Adam Smith <coloradoasmith@gmail.com>
2020-09-10 14:14:53 -0600

    article-service practice working

---
+++ blogful-scripts/seed.blogful_articles.sql
@@ -0,0 +1,24 @@
+INSERT INTO blogful_articles (title, date_published, content)
+VALUES
+    ('Article One', now() - '69 days'::INTERVAL, 'some stupid crap goes here'),
+    ('Article Two', now() - '59 days'::INTERVAL, 'some stupid crap goes here'),
+    ('Article Three', now() - '49 days'::INTERVAL, 'some stupid crap goes here'),
+    ('Article 4', now() - '47 days'::INTERVAL, 'some stupid crap goes here'),
+    ('Article 5', now() - '46 days'::INTERVAL, 'some stupid crap goes here'),
+    ('Article 5', now() - '43 days'::INTERVAL, 'some stupid crap goes here'),
+    ('Article 6', now() - '41 days'::INTERVAL, 'some stupid crap goes here'),
+    ('Article 7', now() - '40 days'::INTERVAL, 'some stupid crap goes here'),
+    ('Article O8', now() - '39 days'::INTERVAL, 'some stupid crap goes here'),
+    ('Article 9', now() - '38 days'::INTERVAL, 'some stupid crap goes here'),
+    ('Article Ten', now() - '37 days'::INTERVAL, 'some stupid crap goes here'),
+    ('Article 11', now() - '30 days'::INTERVAL, 'some stupid crap goes here'),
+    ('Article 12', now() - '25 days'::INTERVAL, 'some stupid crap goes here'),
+    ('Article 13', now() - '23 days'::INTERVAL, 'some stupid crap goes here'),
+    ('Article 14', now() - '21 days'::INTERVAL, 'some stupid crap goes here'),
+    ('Article 15', now() - '20 days'::INTERVAL, 'some stupid crap goes here'),
+    ('Article 16', now() - '19 days'::INTERVAL, 'some stupid crap goes here'),
+    ('Article 17', now() - '17 days'::INTERVAL, 'some stupid crap goes here'),
+    ('Article 18', now() - '15 days'::INTERVAL, 'some stupid crap goes here'),
+    ('Article 19', now() - '13 days'::INTERVAL, 'some stupid crap goes here'),
+    ('Article 20', now() - '3 days'::INTERVAL, 'some stupid crap goes here')
+    ;
